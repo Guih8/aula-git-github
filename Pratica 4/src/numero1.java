7fa3f7d666fe9a3cb95113364944e1d51a8220f0
Guilherme Loureiro <guilhermeloureiro.1626@aluno.una.br>
2022-03-29 21:32:19 -0300

    Pratica4

---
+++ Pratica 4/src/numero1.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class numero1 {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		double nota1, nota2, media;
+	
+		System.out.println("Digite a primeira nota");
+		nota1 = input.nextDouble();
+		System.out.println("Digite a segunda nota");
+		nota2 = input.nextDouble();
+	
+		media = (nota1 + nota2)/2;
+	if(media>=7)
+	System.out.println("Você foi (Aprovado!!!!!!)");
+	else if(media<7)
+	System.out.println("Você Foi (Reprovado!!!!!!)");
+	
+	
+		
+	}
+ 
+
+}
