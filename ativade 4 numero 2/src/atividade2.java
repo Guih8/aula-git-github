6d73c6421984b4e1c840fba9a7bd26c50ab91364
Guih8 <102630791+Guih8@users.noreply.github.com>
2022-03-29 21:49:14 -0300

    Update atividade2.java

--- ativade 4 numero 2/src/atividade2.java
+++ ativade 4 numero 2/src/atividade2.java
@@ -1,9 +1,27 @@
+import java.util.Scanner;
 
-public class atividade2 {
+public class numero2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+		Scanner input = new Scanner(System.in);
+		
+		double lado1, lado2, lado3;
+		
+		System.out.println("Digite os lados do triangulo");
+		lado1 = input.nextDouble();
+		lado2 = input.nextDouble();
+		lado3 = input.nextDouble();
+		
+		
+		if (lado1 == lado2 && lado2 == lado3)
+			System.out.println("Isosceles");
+		else if (lado1 == lado2 || lado1 == lado3 || lado2 == lado3)
+		System.out.println("Equilatero");
+		else
+			System.out.println("Escaleno");
+		
+			
 	}
 
 }
